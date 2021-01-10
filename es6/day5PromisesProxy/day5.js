25f4f85017eee50b7c9e615d9e2e5ccea9d16b8c
Your Name <you@example.com>
2021-01-11 01:56:19 +0530

    update1

--- es6/day5PromisesProxy/day5.js
+++ es6/day5PromisesProxy/day5.js
@@ -1,82 +1,3 @@
-// //function for GET products
-// function getProducts() {
-//   return new Promise((resolve, reject) => {
-//     let xhr = new XMLHttpRequest();
-
-//     // subscribe to the responses for success and failure
-//     // success
-//     xhr.onload = function () {
-//       // check for Http Status as 200
-//       if (xhr.status == 200) {
-//         console.log(`In onload ${xhr.response}`);
-//         // resolve and notify the response to client / subscriber
-//         resolve(xhr.response);
-//       } else {
-//         // reject if there is different status code
-//         reject("Some Error occurred with the status code");
-//       }
-//     };
-
-//     // failure
-//     xhr.onerror = function () {
-//       // reject if there is different status code
-//       reject("Some Error Occurred with Http Communication");
-//     };
-
-//     // initiate the request
-//     xhr.open(
-//       "GET",
-//       "https://apiapptrainingnewapp.azurewebsites.net/api/Products"
-//     );
-//     // send the request
-//     xhr.send();
-//   });
-// }
-
-// // subscribe to the Promise object and either get Resolve or rejected//
-// getProducts()
-//   .then((response) => {
-//     console.log(`Received Response ${response}`);
-//   })
-//   .catch((error) => {
-//     console.log(`Received Error ${error}`);
-//   });
-
-// //function for POST data
-// function postProduct(url, data) {
-//   return new Promise((resolve, reject) => {
-//     let request = new XMLHttpRequest();
-
-//     request.onload = function () {
-//       if (request.status == 201) {
-//         resolve(request.response);
-//       } else {
-//         reject(new Error(request.statusText));
-//       }
-//     };
-
-//     request.onerror = function () {
-//       reject(new Error("May ne Network Error"));
-//     };
-
-//     request.open("POST", url); // PUT  // DELETE
-//     // define the request header
-//     request.setRequestHeader("Content-Type", "application/json");
-//     // pass the data as JSON string
-//     request.send(JSON.stringify(data));
-//   });
-// }
-
-// let prdData = {
-//   ProductRowId: "",
-//   ProductId: "",
-//   ProductName: "",
-//   CategoryName: "",
-//   Manufacturer: "",
-//   Description: "",
-//   Price: 0,
-// };
-
 let prdData = {};
 
 //validation using proxy
@@ -87,16 +8,25 @@ const saveClickHandler = {
     //   `${property} == "ProductName" && ${value.length} > 6 :>> `,
     //   property == "ProductName" && value.length > 6
     // );
+
     if (property == "ProductId" && value.length < 1) {
       document.getElementById("display").innerHTML = "Must enter a product ID";
       return false;
     }
-    if (property == "ProductName" && value.length > 6) {
+    if (property == "ProductRowId" && value.length < 1) {
       document.getElementById("display").innerHTML =
-        "Product name cannot exceed 30 characters";
+        "Must enter a product Row ID";
       return false;
     }
-    if (property == "Price" && (isNaN(value) || parseInt(value) < 0)) {
+    if (property == "ProductName" && (value.length < 1 || value.length > 10)) {
+      document.getElementById("display").innerHTML =
+        "Product name is Must and should not exceed 30 characters";
+      return false;
+    }
+    if (
+      property == "Price" &&
+      (isNaN(value) || parseInt(value) < 0 || value.length < 1)
+    ) {
       document.getElementById("display").innerHTML =
         "Price must be a positive Integer";
       return false;
@@ -111,19 +41,209 @@ prdData = new Proxy(prdData, saveClickHandler);
 
 function onClickHandler() {
   prdData.Price = document.getElementById("Price").value;
+  console.log(prdData.Price);
+  prdData.Description = document.getElementById("Description").value;
   prdData.CategoryName = document.getElementById("CategoryName").value;
   prdData.Manufacturer = document.getElementById("Manufacturer").value;
   prdData.ProductName = document.getElementById("ProductName").value;
-  prdData.ProductId = getValueById("ProductId");
+  console.log(prdData.ProductName);
+  prdData.ProductId = document.getElementById("ProductId").value;
+  console.log(prdData.ProductId);
   prdData.ProductRowId = document.getElementById("ProductRowId").value;
+  console.log(prdData.ProductRowId);
   console.log(Object.values(prdData));
+
+  if (Object.values(prdData).length < 7) {
+    document.getElementById("post").disabled = true;
+    document.getElementById("put").disabled = true;
+    document.getElementById("get").disabled = true;
+    document.getElementById("delete").disabled = true;
+  } else {
+    document.getElementById("post").disabled = false;
+    document.getElementById("put").disabled = false;
+    document.getElementById("get").disabled = false;
+    document.getElementById("delete").disabled = false;
+  }
+}
+//--------------------------------------xx-----------------------------------------------------
+//function for GET products
+function getProducts() {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+
+    // subscribe to the responses for success and failure
+    // success
+    xhr.onload = function () {
+      // check for Http Status as 200
+      if (xhr.status == 200) {
+        // console.log(`In onload ${xhr.response}`);
+        // resolve and notify the response to client / subscriber
+        resolve(xhr.response);
+      } else {
+        // reject if there is different status code
+        reject("Some Error occurred with the status code");
+      }
+    };
+    // failure
+    xhr.onerror = function () {
+      // reject if there is different status code
+      reject("Some Error Occurred with Http Communication");
+    };
+
+    // initiate the request
+    xhr.open(
+      "GET",
+      "https://apiapptrainingnewapp.azurewebsites.net/api/Products"
+    );
+    // send the request
+    xhr.send();
+  });
 }
 
-// prdData.ProductID = "123";
-// console.log(prdData.ProductID);
-// console.log(prdData);
+// subscribe to the Promise object and either get Resolve or rejected//
+document.getElementById("get").addEventListener(onclick, function () {
+  getProducts()
+    .then((response) => {
+      console.log(`Received Response ${response}`);
+    })
+    .catch((error) => {
+      console.log(`Received Error ${error}`);
+    });
+});
 
-//validation using promises
+//---------------------------------------------xx----------------------------------------
+
+//function for POST data
+function postProduct(url, data) {
+  return new Promise((resolve, reject) => {
+    let request = new XMLHttpRequest();
+
+    request.onload = function () {
+      if (request.status == 201) {
+        resolve(request.response);
+      } else {
+        reject(new Error(request.statusText));
+      }
+    };
+
+    request.onerror = function () {
+      reject(new Error("May be Network Error"));
+    };
+
+    request.open("POST", url); // PUT  // DELETE
+    // define the request header
+    request.setRequestHeader("Content-Type", "application/json");
+    // pass the data as JSON string
+    request.send(JSON.stringify(data));
+  });
+}
+
+document.getElementById("post").addEventListener(onclick, function () {
+  postProduct(
+    "https://apiapptrainingnewapp.azurewebsites.net/api/Products",
+    prdData
+  )
+    .then((resp) => {
+      console.log(`Data Created ${resp}`);
+    })
+    .catch((error) => {
+      console.log(`Data Creation failed ${error}`);
+    });
+});
+
+// URL for PUT
+// <id>  is a value based on which record will be searched
+// "https://apiapptrainingnewapp.azurewebsites.net/api/Products/<id>"
+// PUT Request also accepts data in Body as JSON
+//  request.send(JSON.stringify(data));
+
+// URL for DELETE
+// <id>  is a value based on which record will be searched and deleted
+// "https://apiapptrainingnewapp.azurewebsites.net/api/Products/<id>"
+
+// ------------------------------------------xx--------------------------------------------------
+
+function putProduct(url, data) {
+  return new Promise((resolve, reject) => {
+    let request = new XMLHttpRequest();
+
+    request.onload = function () {
+      if (request.status == 201) {
+        resolve(request.response);
+      } else {
+        reject(new Error(request.statusText));
+      }
+    };
+
+    request.onerror = function () {
+      reject(new Error("May be Network Error"));
+    };
+
+    request.open("PUT", url); // PUT  // DELETE
+    // define the request header
+    request.setRequestHeader("Content-Type", "application/json");
+    // pass the data as JSON string
+    request.send(JSON.stringify(data));
+  });
+}
+
+//subscribe to the Promise object and either get Resolve or rejected function
+document.getElementById("put").addEventListener(onclick, function () {
+  putProduct(
+    "https://apiapptrainingnewapp.azurewebsites.net/api/Products" +
+      `${prdData.ProductRowId}`,
+    prdData
+  )
+    .then((resp) => {
+      console.log(`Data Updated ${resp}`);
+    })
+    .catch((error) => {
+      console.log(`Data Updation failed ${error}`);
+    });
+});
+
+//-----------------------------------------------------xx--------------------------------------------
+function deleteProduct(url, data) {
+  return new Promise((resolve, reject) => {
+    let request = new XMLHttpRequest();
+
+    request.onload = function () {
+      if (request.status == 201) {
+        resolve(request.response);
+      } else {
+        reject(new Error(request.statusText));
+      }
+    };
+
+    request.onerror = function () {
+      reject(new Error("May ne Network Error"));
+    };
+
+    request.open("DELETE", url); // PUT  // DELETE
+    // define the request header
+    request.setRequestHeader("Content-Type", "application/json");
+    // pass the data as JSON string
+    request.send(JSON.stringify(data));
+  });
+}
+
+//subscribe to the Promise object and either get Resolve or rejected function
+document.getElementById("delete").addEventListener(onclick, function () {
+  deleteProduct(
+    "https://apiapptrainingnewapp.azurewebsites.net/api/Products" +
+      `${prdData.ProductRowId}`,
+    prdData
+  )
+    .then((resp) => {
+      console.log(`Data deleted ${resp}`);
+    })
+    .catch((error) => {
+      console.log(`Data deletion failed ${error}`);
+    });
+});
+
+//-------------------------------------------------extra-----------------------------------------
+// validation using promises
 // function validateForm() {
 //   return new Promise((resolve, reject) => {
 //     if (getValueById("ProductId").length < 1) {
@@ -160,82 +280,6 @@ function onClickHandler() {
 //     });
 // }
 
-function getValueById(id) {
-  return document.getElementById(id).value;
-}
-
-// // let prdData = {
-// //   ProductId: "Prd998",
-// //   ProductName: "Pen",
-// //   CategoryName: "Stationary",
-// //   Manufacturer: "Reynolds",
-// //   Description: "Gel-Pen",
-// //   BasePrice: 40,
-// // };
-
-// // subscribe to the Promise object and either get Resolve or rejected
-// postProduct(
-//   "https://apiapptrainingnewapp.azurewebsites.net/api/Products",
-//   prdData
-// )
-//   .then((resp) => {
-//     console.log(`Data Created ${resp}`);
-//   })
-//   .catch((error) => {
-//     console.log(`Data Creation failed ${error}`);
-//   });
-
-// // URL for PUT
-// // <id>  is a value based on which record will be searched
-// // "https://apiapptrainingnewapp.azurewebsites.net/api/Products/<id>"
-// // PUT Request also accepts data in Body as JSON
-// //  request.send(JSON.stringify(data));
-
-// // URL for DELETE
-// // <id>  is a value based on which record will be searched and deleted
-// // "https://apiapptrainingnewapp.azurewebsites.net/api/Products/<id>"
-
-// function putProduct(url, data) {
-//   return new Promise((resolve, reject) => {
-//     let request = new XMLHttpRequest();
-
-//     request.onload = function () {
-//       if (request.status == 201) {
-//         resolve(request.response);
-//       } else {
-//         reject(new Error(request.statusText));
-//       }
-//     };
-
-//     request.onerror = function () {
-//       reject(new Error("May ne Network Error"));
-//     };
-
-//     request.open("PUT", url); // PUT  // DELETE
-//     // define the request header
-//     request.setRequestHeader("Content-Type", "application/json");
-//     // pass the data as JSON string
-//     request.send(JSON.stringify(data));
-//   });
+// function getValueById(id) {
+//   return document.getElementById(id).value;
 // }
-
-// let prdData = {
-//   ProductId: "Prd998",
-//   ProductName: "Pen",
-//   CategoryName: "Stationary",
-//   Manufacturer: "Renolds",
-//   Description: "Gel-Pen",
-//   BasePrice: 40,
-// };
-
-// // subscribe to the Promise object and either get Resolve or rejected
-// postProduct(
-//   "https://apiapptrainingnewapp.azurewebsites.net/api/Products",
-//   prdData
-// )
-//   .then((resp) => {
-//     console.log(`Data Created ${resp}`);
-//   })
-//   .catch((error) => {
-//     console.log(`Data Creation failed ${error}`);
-//   });
